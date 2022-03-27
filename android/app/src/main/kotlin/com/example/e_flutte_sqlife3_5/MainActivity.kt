99b50f3df2d31bf64be61e0b5b2c01e69f8bda04
Vong Phanit <VPhanit@dli-cambodia.local>
2022-03-27 16:08:25 +0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/e_flutte_sqlife3_5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_flutte_sqlife3_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/show_people_page.dart
@@ -0,0 +1,75 @@
+import 'package:e_flutte_sqlife3_5/controllers/people_controller.dart';
+import 'package:e_flutte_sqlife3_5/models/people_model.dart';
+import 'package:e_flutte_sqlife3_5/views/create_people_page.dart';
+import 'package:flutter/material.dart';
+
+class ShowPeoplePage extends StatefulWidget {
+  const ShowPeoplePage({Key? key}) : super(key: key);
+
+  @override
+  State<ShowPeoplePage> createState() => _ShowPeoplePageState();
+}
+
+class _ShowPeoplePageState extends State<ShowPeoplePage> {
+  @override
+  void initState() {
+    super.initState();
+    selectPeople();
+  }
+
+  List<PeopleModel> list = [];
+  bool status = false;
+
+  selectPeople() async {
+    list = await PeopleController().selectPeople();
+    if (list.isEmpty) {
+      setState(() {
+        status = false;
+      });
+    } else {
+      setState(() {
+        status = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List People'),
+      ),
+      body: Visibility(
+        visible: true,
+        child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            var data = list[index];
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Text(data.id.toString()),
+                ),
+                title: Text(data.name),
+                subtitle: Text(data.address),
+                trailing: Text(data.gender),
+              ),
+            );
+          },
+        ),
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePeoplePage()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
